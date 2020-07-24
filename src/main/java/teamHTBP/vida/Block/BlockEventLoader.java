9681e5bc4760ccc4b38cfa87a114eaf69d9419e5
DespairP <DespairP@users.noreply.github.com>
2020-07-25 00:55:53 +0800

    hud

--- src/main/java/teamHTBP/vida/Block/BlockEventLoader.java
+++ src/main/java/teamHTBP/vida/Block/BlockEventLoader.java
@@ -1,13 +1,42 @@
 package teamHTBP.vida.Block;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileHelper;
+import net.minecraft.util.math.*;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ObjectHolder;
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+import teamHTBP.vida.Block.function.BlockPurfiedCauldron;
+import teamHTBP.vida.TileEntity.TileEntityPurfiedCauldron;
+import teamHTBP.vida.gui.HUD.PurfiedCauldronHUD;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber
 public class BlockEventLoader {
 
+    //锅的HUD
+    @SubscribeEvent
+    public static void onOverlayRender(RenderGameOverlayEvent event) {
+        if (event.getType() != RenderGameOverlayEvent.ElementType.ALL) {
+            return;
+        }
+        if (Minecraft.getInstance().player == null ) {
+            return;
+        }
+        PlayerEntity player = Minecraft.getInstance().player;
+        RayTraceResult objectMouseOver = Minecraft.getInstance().objectMouseOver;
+        BlockPos pos = new BlockPos(objectMouseOver.getHitVec());
+        Block block = player.world.getBlockState(pos).getBlock();
+        if(block instanceof BlockPurfiedCauldron){
+            TileEntityPurfiedCauldron tileEntityPurfiedCauldron = (TileEntityPurfiedCauldron)player.world.getTileEntity(pos);
+            PurfiedCauldronHUD purfiedCauldronHUD = new PurfiedCauldronHUD(tileEntityPurfiedCauldron);
+            purfiedCauldronHUD.render();
+        }
+    }
 }
---
+++ src/main/java/teamHTBP/vida/gui/HUD/PurfiedCauldronHUD.java
@@ -0,0 +1,46 @@
+package teamHTBP.vida.gui.HUD;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.AbstractGui;
+import net.minecraft.util.ResourceLocation;
+import teamHTBP.vida.TileEntity.TileEntityPurfiedCauldron;
+import teamHTBP.vida.Vida;
+
+public class PurfiedCauldronHUD extends AbstractGui {
+    private final int width;
+    private final int height;
+    private final Minecraft minecraft;
+    private final ResourceLocation HUD = new ResourceLocation(Vida.modId, "textures/gui/hud.png");
+    private final TileEntityPurfiedCauldron tileEntityPurfiedCauldron;
+    public PurfiedCauldronHUD(TileEntityPurfiedCauldron tileEntityPurfiedCauldron){
+        width = Minecraft.getInstance().getMainWindow().getScaledWidth();
+        height = Minecraft.getInstance().getMainWindow().getScaledHeight();
+        minecraft = Minecraft.getInstance();
+        this.tileEntityPurfiedCauldron = tileEntityPurfiedCauldron;
+    }
+
+    public void render(){
+        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+        this.minecraft.getTextureManager().bindTexture(HUD);
+        int screenWidth =this.width / 2 - 6;
+        int screenHeight = this.height / 2 - 20;
+        blit(screenWidth,screenHeight,0, 25,16, 14,10,64,64);
+        blit(screenWidth + 3,screenHeight - 10,0, 28,6, 8,5,64,64);
+        if(tileEntityPurfiedCauldron.isWater){
+            blit(screenWidth + 20,screenHeight ,0, 52,2, 7,12,64,64);
+        }else{
+            blit(screenWidth + 20,screenHeight ,0, 52,34, 7,12,64,64);
+        }
+        if(tileEntityPurfiedCauldron.isFire){
+            blit(screenWidth + 20,screenHeight - 15 ,0, 51,18, 12,12,64,64);
+        }else{
+            blit(screenWidth + 20,screenHeight - 15,0, 51,50, 12,12,64,64);
+        }
+        if(!tileEntityPurfiedCauldron.meltItem.isEmpty()){
+            blit(screenWidth + 3,screenHeight - 6,0, 28,11, 8,5,64,64);
+            Minecraft.getInstance().getItemRenderer().renderItemIntoGUI(tileEntityPurfiedCauldron.meltItem, screenWidth - 1, screenHeight - 27);
+
+        }
+    }
+}
