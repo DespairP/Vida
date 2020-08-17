7d22e9c4b4d80fc763f1a07139eabc773d4a5bf2
DespairP <DespairP@users.noreply.github.com>
2020-08-18 01:15:40 +0800

    Code Clean up

--- src/main/java/teamHTBP/vida/particle/ParticleLoader.java
+++ src/main/java/teamHTBP/vida/particle/ParticleLoader.java
@@ -1,12 +1,10 @@
 package teamHTBP.vida.particle;
 
-import net.minecraft.client.particle.Particle;
 import net.minecraft.particles.ParticleType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import teamHTBP.vida.Vida;
-
 
 public class ParticleLoader {
     public final static DeferredRegister<ParticleType<?>> PARTICLE = new DeferredRegister<>(ForgeRegistries.PARTICLE_TYPES, Vida.modId);
