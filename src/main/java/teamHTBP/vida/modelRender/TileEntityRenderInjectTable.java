62166d8b7a7048f039217214809f987c9781f608
TT432 <2437315224@qq.com>
2021-08-24 22:50:33 +0800

    edit InjectTableTileEntityRenderer

--- src/main/java/teamHTBP/vida/modelRender/TileEntityRenderInjectTable.java
+++ src/main/java/teamHTBP/vida/modelRender/TileEntityRenderInjectTable.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
@@ -14,14 +15,25 @@ import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.item.SwordItem;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.math.vector.Quaternion;
+import net.minecraft.util.math.vector.Vector3f;
 import teamHTBP.vida.TileEntity.TileEntityInjectTable;
+import teamHTBP.vida.Vida;
 import teamHTBP.vida.client.ClientTickHandler;
+import teamHTBP.vida.modelRender.tilemodel.InjectTableModel;
 
 public class TileEntityRenderInjectTable extends TileEntityRenderer<TileEntityInjectTable> {
-    double sinWave() {
-        return (ClientTickHandler.tick() * 0.1) % (Math.PI * 2);
+    public static final ResourceLocation TEXTURE = new ResourceLocation(Vida.modId, "textures/tileentity/injecttable.png");
+    public static final InjectTableModel MODEL = new InjectTableModel();
+
+    float tick(float partTicks) {
+        return ClientTickHandler.tick() + partTicks;
+    }
+
+    double sinWave(float partTicks) {
+        return (tick(partTicks) * 0.1) % (Math.PI * 2);
     }
 
 
@@ -31,100 +43,24 @@ public class TileEntityRenderInjectTable extends TileEntityRenderer<TileEntityIn
 
     @Override
     public void render(TileEntityInjectTable tileEntityIn, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int combinedLightIn, int combinedOverlayIn) {
+        IVertexBuilder iVertexBuilder = bufferIn.getBuffer(RenderType.getEntityTranslucent(TEXTURE));
+
         matrixStackIn.push();
-        TextureAtlasSprite atlasTexture = Minecraft.getInstance().getAtlasSpriteGetter(AtlasTexture.LOCATION_BLOCKS_TEXTURE).apply(RenderLoader.injectTable_top);
-        IVertexBuilder builder = bufferIn.getBuffer(RenderType.getCutout());
-        Matrix4f matrixStack = matrixStackIn.getLast().getMatrix();
+        matrixStackIn.scale(1, -1, 1);
+        matrixStackIn.translate(0.5, 0, 0.5);
 
-
-        matrixStackIn.translate(0.5F, 0.65, 0.5F);
-        matrixStackIn.scale(0.4f, 0.4f, 0.4f);
-        matrixStackIn.rotate(new Quaternion(0, ClientTickHandler.tick() % 360, 0, true));
-        matrixStackIn.translate(-0.5f, -0.5f, -0.5f);
-        float Minu = atlasTexture.getMinU();
-        float Maxu = atlasTexture.getMaxU();
-        float MinV = atlasTexture.getMinV();
-        float MaxV = atlasTexture.getMaxV();
-
-        builder.pos(matrixStack, 0, 1, 0).color(1, 1, 1, 1.0f).tex(Minu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 1, 0).color(1, 1, 1, 1.0f).tex(Maxu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 1, 1).color(1, 1, 1, 1.0f).tex(Maxu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 0, 1, 1).color(1, 1, 1, 1.0f).tex(Minu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-
-
-        builder.pos(matrixStack, 0, 1, 1).color(1, 1, 1, 1.0f).tex(Minu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 1, 1).color(1, 1, 1, 1.0f).tex(Maxu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 1, 0).color(1, 1, 1, 1.0f).tex(Maxu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 0, 1, 0).color(1, 1, 1, 1.0f).tex(Minu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-
-
+        matrixStackIn.push();
+        matrixStackIn.rotate(Vector3f.YN.rotationDegrees(tick(partialTicks)));
+        int light = LightTexture.packLight(10, 10);
+        MODEL.rotateCube.render(matrixStackIn, iVertexBuilder, light, combinedOverlayIn);
         matrixStackIn.pop();
-
-
-        matrixStackIn.push();
-        matrixStack = matrixStackIn.getLast().getMatrix();
-        matrixStackIn.translate(0.5F, 0.70, 0.5F);
-        matrixStackIn.scale(0.4f, 0.15f, 0.4f);
-        matrixStackIn.rotate(new Quaternion(0, ClientTickHandler.tick() % 360, 0, true));
-        matrixStackIn.translate(-0.5f, 0, -0.5f);
-
-        atlasTexture = Minecraft.getInstance().getAtlasSpriteGetter(AtlasTexture.LOCATION_BLOCKS_TEXTURE).apply(RenderLoader.injectTable_side);
-        Minu = atlasTexture.getMinU();
-        Maxu = atlasTexture.getMaxU();
-        MinV = atlasTexture.getMinV();
-        MaxV = atlasTexture.getMaxV();
-
-        builder.pos(matrixStack, 0, 1, 1).color(1, 1, 1, 1.0f).tex(Minu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 1, 1).color(1, 1, 1, 1.0f).tex(Maxu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 0, 1).color(1, 1, 1, 1.0f).tex(Maxu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 0, 0, 1).color(1, 1, 1, 1.0f).tex(Minu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-
-        builder.pos(matrixStack, 0, 0, 1).color(1, 1, 1, 1.0f).tex(Minu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 0, 1).color(1, 1, 1, 1.0f).tex(Maxu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 1, 1).color(1, 1, 1, 1.0f).tex(Maxu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 0, 1, 1).color(1, 1, 1, 1.0f).tex(Minu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-
-        builder.pos(matrixStack, 0, 1, 1).color(1, 1, 1, 1.0f).tex(Minu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 0, 1, 0).color(1, 1, 1, 1.0f).tex(Maxu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 0, 0, 0).color(1, 1, 1, 1.0f).tex(Maxu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 0, 0, 1).color(1, 1, 1, 1.0f).tex(Minu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-
-        builder.pos(matrixStack, 0, 0, 1).color(1, 1, 1, 1.0f).tex(Minu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 0, 0, 0).color(1, 1, 1, 1.0f).tex(Maxu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 0, 1, 0).color(1, 1, 1, 1.0f).tex(Maxu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 0, 1, 1).color(1, 1, 1, 1.0f).tex(Minu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-
-        builder.pos(matrixStack, 0, 0, 1).color(1, 1, 1, 1.0f).tex(Minu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 0, 0, 0).color(1, 1, 1, 1.0f).tex(Maxu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 0, 1, 0).color(1, 1, 1, 1.0f).tex(Maxu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 0, 1, 1).color(1, 1, 1, 1.0f).tex(Minu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-
-        builder.pos(matrixStack, 0, 1, 0).color(1, 1, 1, 1.0f).tex(Minu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 1, 0).color(1, 1, 1, 1.0f).tex(Maxu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 0, 0).color(1, 1, 1, 1.0f).tex(Maxu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 0, 0, 0).color(1, 1, 1, 1.0f).tex(Minu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-
-        builder.pos(matrixStack, 0, 0, 0).color(1, 1, 1, 1.0f).tex(Minu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 0, 0).color(1, 1, 1, 1.0f).tex(Maxu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 1, 0).color(1, 1, 1, 1.0f).tex(Maxu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 0, 1, 0).color(1, 1, 1, 1.0f).tex(Minu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-
-        builder.pos(matrixStack, 1, 1, 1).color(1, 1, 1, 1.0f).tex(Minu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 1, 0).color(1, 1, 1, 1.0f).tex(Maxu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 0, 0).color(1, 1, 1, 1.0f).tex(Maxu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 0, 1).color(1, 1, 1, 1.0f).tex(Minu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-
-        builder.pos(matrixStack, 1, 0, 1).color(1, 1, 1, 1.0f).tex(Minu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 0, 0).color(1, 1, 1, 1.0f).tex(Maxu, MaxV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 1, 0).color(1, 1, 1, 1.0f).tex(Maxu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-        builder.pos(matrixStack, 1, 1, 1).color(1, 1, 1, 1.0f).tex(Minu, MinV).lightmap(150, 240).normal(1, 0, 0).endVertex();
-
+        MODEL.render(matrixStackIn, iVertexBuilder, combinedLightIn, combinedOverlayIn, 1, 1, 1, 1);
 
         matrixStackIn.pop();
 
         if (tileEntityIn.hasSwordItem() && tileEntityIn.getSwordStack().getItem() instanceof SwordItem) {
             matrixStackIn.push();
-            double floating = 0.12 * Math.sin(sinWave());
+            double floating = 0.12 * Math.sin(sinWave(partialTicks));
             matrixStackIn.translate(0.5f, 1.8f + floating, 0.5f);
             matrixStackIn.rotate(new Quaternion(0, 0, 180 - 45, true));
             ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
@@ -133,7 +69,7 @@ public class TileEntityRenderInjectTable extends TileEntityRenderer<TileEntityIn
             matrixStackIn.pop();
         } else if (tileEntityIn.hasSwordItem()) {
             matrixStackIn.push();
-            double floating = 0.12 * Math.sin(sinWave());
+            double floating = 0.12 * Math.sin(sinWave(partialTicks));
             matrixStackIn.translate(0.5f, 1.8f + floating, 0.5f);
             matrixStackIn.rotate(new Quaternion(0, 0, -45, true));
             ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
