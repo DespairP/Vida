42f846427de18b572141c22592702b6205d5c300
DespairP <328060206@qq.com>
2022-05-24 03:23:55 +0800

    fix Block Player Looking at problem

--- src/main/java/teamHTBP/vida/event/client/BlockFunctionHUDEventLoader.java
+++ src/main/java/teamHTBP/vida/event/client/BlockFunctionHUDEventLoader.java
@@ -2,13 +2,17 @@ package teamHTBP.vida.event.client;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -24,7 +28,7 @@ import teamHTBP.vida.item.armor.ItemArmorElementLegginsWithBottles;
 import teamHTBP.vida.item.staff.ItemElementPickaxe;
 import teamHTBP.vida.item.staff.ItemElementSword;
 
-
+@OnlyIn(Dist.CLIENT)
 @Mod.EventBusSubscriber(Dist.CLIENT)
 public class BlockFunctionHUDEventLoader {
     public static int alpha = 0;
@@ -38,40 +42,33 @@ public class BlockFunctionHUDEventLoader {
         if (event.getType() != RenderGameOverlayEvent.ElementType.ALL) {
             return;
         }
-        if (Minecraft.getInstance().player == null) {
-            return;
-        }
-        if (Minecraft.getInstance().world == null) {
+        if (Minecraft.getInstance().player == null || Minecraft.getInstance().world == null) {
             return;
         }
 
         MatrixStack matrixStack = event.getMatrixStack();
-
         PlayerEntity player = Minecraft.getInstance().player;
-        RayTraceResult objectMouseOver = Minecraft.getInstance().objectMouseOver;
-        BlockPos pos = new BlockPos(objectMouseOver.getHitVec());
 
-        Block block = player.world.getBlockState(pos).getBlock();
+        /*
+            RayTraceResult objectMouseOver = Minecraft.getInstance().objectMouseOver;
+            BlockPos pos = new BlockPos(objectMouseOver.getHitVec());
+         */
+
+        Block block = getBlockPlayerLookAt(player);
+        BlockPos lookingPos = getLookingPos(player);
+
         if (block instanceof BlockPurfiedCauldron) {
-            TileEntityPurfiedCauldron tileEntityPurfiedCauldron = (TileEntityPurfiedCauldron) player.world.getTileEntity(pos);
+            TileEntityPurfiedCauldron tileEntityPurfiedCauldron = (TileEntityPurfiedCauldron) player.world.getTileEntity(lookingPos);
             if (tileEntityPurfiedCauldron != null) {
                 PurfiedCauldronHUD purfiedCauldronHUD = new PurfiedCauldronHUD(tileEntityPurfiedCauldron);
                 purfiedCauldronHUD.render(event.getMatrixStack());
             }
             return;
         }
-        Vector3d vec3d = player.getLook(event.getPartialTicks());
-        Vector3d vec3d1 = objectMouseOver.getHitVec();
-
-        float d0 = 1.0f;
-        Vector3d vec3d2 = vec3d.add(vec3d1.x * d0, vec3d1.y * d0, vec3d1.z * d0);
-
-        BlockPos newpos = new BlockPos(vec3d2);
-        block = player.world.getBlockState(newpos).getBlock();
 
         //渲染元素祭坛HUD
         if (block instanceof BlockElementCoreAltar) {
-            TileEntityElementCoreAltar tileEntityElementCoreAltar = (TileEntityElementCoreAltar) player.world.getTileEntity(newpos);
+            TileEntityElementCoreAltar tileEntityElementCoreAltar = (TileEntityElementCoreAltar) player.world.getTileEntity(lookingPos);
             ElementCoreAltarHUD elementCoreAltarHUD = new ElementCoreAltarHUD(tileEntityElementCoreAltar);
             elementCoreAltarHUD.render(matrixStack);
             return;
@@ -79,7 +76,7 @@ public class BlockFunctionHUDEventLoader {
 
         //渲染收集器HUD
         if (block instanceof BlockCollecter) {
-            TileEntityCollector tileEntityCollector = (TileEntityCollector) player.world.getTileEntity(newpos);
+            TileEntityCollector tileEntityCollector = (TileEntityCollector) player.world.getTileEntity(lookingPos);
             CollectorHUD collectorHUD = new CollectorHUD(tileEntityCollector);
             collectorHUD.render(matrixStack);
             return;
@@ -102,23 +99,6 @@ public class BlockFunctionHUDEventLoader {
             }
         }
 
-        //渲染元素镐HUD
-        /*
-        if (player.inventory.getCurrentItem().getItem() instanceof ItemElementPickaxe) {
-            if (alpha < 100) alpha += 2;
-            itemStack = player.inventory.getCurrentItem();
-        } else {
-            if (alpha > 0) alpha -= 2;
-        }
-        if (alpha > 0 && itemStack != ItemStack.EMPTY && !itemStack.isEmpty()) {
-            //ItemStack stack = player.inventory.getCurrentItem();
-            ElementPickaxeHUD hud = new ElementPickaxeHUD(itemStack, alpha);
-            hud.render(matrixStack);
-        }
-        if (alpha == 0) {
-            itemStack = ItemStack.EMPTY;
-        }*/
-
         //渲染元素剑HUD
         if (player.inventory.getCurrentItem().getItem() instanceof ItemElementSword) {
             if (sword_alpha < 100) sword_alpha += 2;
@@ -132,9 +112,27 @@ public class BlockFunctionHUDEventLoader {
             ElementSwordHUD hud = new ElementSwordHUD(itemStack1, sword_alpha);
             hud.render(matrixStack);
         }
-
-
     }
 
 
+    @OnlyIn(Dist.CLIENT)
+    public static Block getBlockPlayerLookAt(PlayerEntity player){
+        RayTraceResult block =  player.pick(20.0D, 0.0F, false);
+        if(block.getType() == RayTraceResult.Type.BLOCK) {
+            BlockPos blockpos = ((BlockRayTraceResult)block).getPos();
+            BlockState blockstate = player.world.getBlockState(blockpos);
+            return blockstate.getBlock();
+        }
+        return null;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static BlockPos getLookingPos(PlayerEntity player){
+        RayTraceResult block =  player.pick(20.0D, 0.0F, false);
+        if(block.getType() == RayTraceResult.Type.BLOCK) {
+            BlockPos blockpos = ((BlockRayTraceResult) block).getPos();
+            return blockpos;
+        }
+        return new BlockPos(block.getHitVec());
+    }
 }
