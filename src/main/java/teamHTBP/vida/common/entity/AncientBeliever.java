d7852867e789a55de4eaf549486f70b752e78fd4
TT432 <2437315224@qq.com>
2022-08-03 18:34:15 +0800

    行走动画修复

--- src/main/java/teamHTBP/vida/common/entity/AncientBeliever.java
+++ src/main/java/teamHTBP/vida/common/entity/AncientBeliever.java
@@ -1,5 +1,8 @@
 package teamHTBP.vida.common.entity;
 
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.PathfinderMob;
@@ -21,6 +24,20 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
  * @author DustW
  */
 public class AncientBeliever extends PathfinderMob implements IAnimatable, IAnimationTickable {
+
+    private static final EntityDataAccessor<Boolean> GOAL_FLAG_MOVE =
+            SynchedEntityData.defineId(AncientBeliever.class, EntityDataSerializers.BOOLEAN);
+    private static final EntityDataAccessor<Boolean> GOAL_FLAG_JUMP =
+            SynchedEntityData.defineId(AncientBeliever.class, EntityDataSerializers.BOOLEAN);
+    private static final EntityDataAccessor<Boolean> GOAL_FLAG_TARGET =
+            SynchedEntityData.defineId(AncientBeliever.class, EntityDataSerializers.BOOLEAN);
+    private static final EntityDataAccessor<Boolean> GOAL_FLAG_LOOK =
+            SynchedEntityData.defineId(AncientBeliever.class, EntityDataSerializers.BOOLEAN);
+
+    private static final EntityDataAccessor<Boolean>[] FLAGS = new EntityDataAccessor[] {
+            GOAL_FLAG_MOVE, GOAL_FLAG_LOOK, GOAL_FLAG_JUMP, GOAL_FLAG_TARGET
+    };
+
     private final AnimationFactory factory = new AnimationFactory(this);
 
     public AncientBeliever(EntityType<AncientBeliever> pEntityType, Level pLevel) {
@@ -39,6 +56,28 @@ public class AncientBeliever extends PathfinderMob implements IAnimatable, IAnim
         this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
     }
 
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+
+        for (var flag : FLAGS) {
+            this.entityData.define(flag, false);
+        }
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        if (!level.isClientSide) {
+            Goal.Flag[] values = Goal.Flag.values();
+
+            for (int i = 0; i < values.length; i++) {
+                entityData.set(FLAGS[i], has(values[i]));
+            }
+        }
+    }
+
     public static AttributeSupplier.Builder createAttributes() {
         return Mob.createMobAttributes()
                 .add(Attributes.MAX_HEALTH, 20.0D)
@@ -48,18 +87,24 @@ public class AncientBeliever extends PathfinderMob implements IAnimatable, IAnim
     @Override
     public void registerControllers(AnimationData data) {
         data.addAnimationController(new AnimationController<>(this, "standby_controller",
-                5, this::standbyPredicate));
+                0, this::standbyPredicate));
 
         data.addAnimationController(new AnimationController<>(this, "walk_controller",
-                5, this::walkPredicate));
+                0, this::walkPredicate));
     }
 
     boolean has(Goal.Flag flag) {
-        return goalSelector.getRunningGoals().anyMatch(g -> g.getFlags().contains(flag));
+        var goals = goalSelector.getAvailableGoals();
+        return goals.stream().anyMatch(g ->
+                g.isRunning() && g.getFlags().contains(flag));
+    }
+
+    boolean has(EntityDataAccessor<Boolean> flag) {
+        return entityData.get(flag);
     }
 
     private <T extends IAnimatable> PlayState standbyPredicate(AnimationEvent<T> event) {
-        if (has(Goal.Flag.MOVE) || has(Goal.Flag.JUMP)) {
+        if (has(GOAL_FLAG_MOVE) || has(GOAL_FLAG_JUMP)) {
             return PlayState.STOP;
         }
         else {
@@ -70,7 +115,7 @@ public class AncientBeliever extends PathfinderMob implements IAnimatable, IAnim
     }
 
     private <T extends IAnimatable> PlayState walkPredicate(AnimationEvent<T> event) {
-        if (has(Goal.Flag.MOVE)) {
+        if (has(GOAL_FLAG_MOVE)) {
             event.getController().setAnimation(new AnimationBuilder()
                     .addAnimation("animation.ancient_believer.walk", true));
             return PlayState.CONTINUE;
